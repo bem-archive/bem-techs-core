d0acb21de25ed88f6948ea21943801e8a953e2b6
Serhii Tatarintsev <sevinf@yandex-team.ru>
2013-08-08 19:04:34 +0300

    Complete browser.js test

--- test/browser.js.test.js
+++ test/browser.js.test.js
@@ -1,28 +1,42 @@
 var BEMSmoke = require('bem-smoke');
 
-describe.skip('browser.js tech', function() {
+describe('browser.js tech', function() {
     var tech;
     beforeEach(function() {
         tech = BEMSmoke.testTech(require.resolve('../techs/browser.js.js'))
                        .withTechMap({
-                           'vanilla.js': require.resolve('../lib/vanilla.js.js'),
+                           'vanilla.js': require.resolve('../techs/vanilla.js.js'),
                            'js': require.resolve('bem/lib/techs/v2/js.js')
                        });
     });
 
     it('builds js file with browser.js', function(done) {
         tech.withSourceFiles({
+            'ym': {
+                'ym.js': '//ym chunk'
+            },
             'block': {
                 'block.browser.js': '//browser'
             }
+        })
+        .withMockedModulesResolves({
+            'ym': '/ym/ym.js'
         })
         .build('/out', {
             deps: [
                 {block: 'block'}
             ]
         })
-        .producesFile('/out.browser.js')
-        .withContent('')
+        .producesFile('/out.js')
+        .withContent('/* ym/ym.js: begin */ /**/',
+                     '//ym chunk;',
+                     '/* ym/ym.js: end */ /**/',
+                     '',
+                     '/* block/block.browser.js: begin */ /**/',
+                     '//browser;',
+                     '/* block/block.browser.js: end */ /**/',
+                     '',
+                     '')
         .notify(done);
     });
 });
