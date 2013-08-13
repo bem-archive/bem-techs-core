8cc3840a8c77076836a2926e96689f3c4bf74841
Serhii Tatarintsev <sevinf@yandex-team.ru>
2013-08-13 17:39:42 +0300

    Remove examples and tests techs

--- techs/examples.js
+++
@@ -1,8 +0,0 @@
-var PATH = require('path'),
-    environ = require('bem-environ');
-
-exports.baseTechPath = environ.getLibPath('bem-pr', 'bem/techs/examples.js');
-
-exports.getBaseLevel = function() {
-    return PATH.resolve(__dirname, '../levels/bundles.js');
-};
